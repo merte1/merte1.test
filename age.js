1f21ba23942c4b19898ded5a09b493e2d71530f2
merte1 <merte@koc.com.tr>
2023-08-07 18:31:50 +0300

    .

--- age.js
+++ age.js
@@ -1,4 +1,4 @@
-define(['pipAPI', '/IAT-TRR.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', window.sourceBase + '/IAT-TRR.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 var posWords = API.shuffle([
             'Aşk', 'Neşe', 'Arkadaş', 'Keyif', 
