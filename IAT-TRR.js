565ad444fee158e6d756c46f6344f33b4ca60545
merte1 <merte@koc.com.tr>
2023-11-22 06:28:41 +0300

    .

--- IAT-TRR.js
+++ IAT-TRR.js
@@ -118,27 +118,17 @@ define(['pipAPI', 'pipScorer', 'underscore'], function (APIConstructor, Scorer, 
 
 			////In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
 			////If you set the number of trials in any block to 0, that block will be skipped.
-			blockAttributes_nTrials: 2,
-			blockAttributes_nMiniBlocks: 2,
-			blockCategories_nTrials: 2,
-			blockCategories_nMiniBlocks: 2,
-			blockFirstCombined_nTrials: 2,
-			blockFirstCombined_nMiniBlocks: 2,
-			blockSecondCombined_nTrials: 2, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
-			blockSecondCombined_nMiniBlocks: 2,
-			blockSwitch_nTrials: 2,
-			blockSwitch_nMiniBlocks: 2,
 
-			// blockAttributes_nTrials: 20,
-			// blockAttributes_nMiniBlocks: 5,
-			// blockCategories_nTrials: 20,
-			// blockCategories_nMiniBlocks: 5,
-			// blockFirstCombined_nTrials: 20,
-			// blockFirstCombined_nMiniBlocks: 5,
-			// blockSecondCombined_nTrials: 40, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
-			// blockSecondCombined_nMiniBlocks: 10,
-			// blockSwitch_nTrials: 28,
-			// blockSwitch_nMiniBlocks: 7,
+			blockAttributes_nTrials: 20,
+			blockAttributes_nMiniBlocks: 5,
+			blockCategories_nTrials: 20,
+			blockCategories_nMiniBlocks: 5,
+			blockFirstCombined_nTrials: 20,
+			blockFirstCombined_nMiniBlocks: 5,
+			blockSecondCombined_nTrials: 40, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
+			blockSecondCombined_nMiniBlocks: 10,
+			blockSwitch_nTrials: 28,
+			blockSwitch_nMiniBlocks: 7,
 
 			//Should we randomize which attribute is on the right, and which on the left?
 			randomAttSide: false, // Accepts 'true' and 'false'. If false, then attribute2 on the right.
@@ -1342,20 +1332,20 @@ define(['pipAPI', 'pipScorer', 'underscore'], function (APIConstructor, Scorer, 
 		function getFB(inText, categoryA, categoryB) {
 			var retText = inText.replace(/attribute1/g, att1.displayInResultAs);
 			retText = retText.replace(/attribute2/g, att2.displayInResultAs);
-			retText = retText.replace(/categoryA/g, categoryA.displayInResultAs);
-			retText = retText.replace(/categoryB/g, categoryB.displayInResultAs);
+			retText = retText.replace(/categoryA/g, categoryA);
+			retText = retText.replace(/categoryB/g, categoryB);
 			return retText;
 		}
 
 		//Set the feedback messages.
 		var messageDef = [
-			{ cut: '-0.65', message: getFB(piCurrent.fb_strong_Att1WithCatA_Att2WithCatB, cat1.name, cat2.name) },
-			{ cut: '-0.35', message: getFB(piCurrent.fb_moderate_Att1WithCatA_Att2WithCatB, cat1.name, cat2.name) },
-			{ cut: '-0.15', message: getFB(piCurrent.fb_slight_Att1WithCatA_Att2WithCatB, cat1.name, cat2.name) },
-			{ cut: '0.15', message: getFB(piCurrent.fb_equal_CatAvsCatB, cat1.name, cat2.name) },
-			{ cut: '0.35', message: getFB(piCurrent.fb_slight_Att1WithCatA_Att2WithCatB, cat2.name, cat1.name) },
-			{ cut: '0.65', message: getFB(piCurrent.fb_moderate_Att1WithCatA_Att2WithCatB, cat2.name, cat1.name) },
-			{ cut: '5', message: getFB(piCurrent.fb_strong_Att1WithCatA_Att2WithCatB, cat2.name, cat1.name) }
+			{ cut: '-0.65', message: getFB(piCurrent.fb_strong_Att1WithCatA_Att2WithCatB, cat1.displayInResultAs, cat2.displayInResultAs) },
+			{ cut: '-0.35', message: getFB(piCurrent.fb_moderate_Att1WithCatA_Att2WithCatB, cat1.displayInResultAs, cat2.displayInResultAs) },
+			{ cut: '-0.15', message: getFB(piCurrent.fb_slight_Att1WithCatA_Att2WithCatB, cat1.displayInResultAs, cat2.displayInResultAs) },
+			{ cut: '0.15', message: getFB(piCurrent.fb_equal_CatAvsCatB, cat1.displayInResultAs, cat2.displayInResultAs) },
+			{ cut: '0.35', message: getFB(piCurrent.fb_slight_Att1WithCatA_Att2WithCatB, cat2.displayInResultAs, cat1.displayInResultAs) },
+			{ cut: '0.65', message: getFB(piCurrent.fb_moderate_Att1WithCatA_Att2WithCatB, cat2.displayInResultAs, cat1.displayInResultAs) },
+			{ cut: '5', message: getFB(piCurrent.fb_strong_Att1WithCatA_Att2WithCatB, cat2.displayInResultAs, cat1.displayInResultAs) }
 		];
 		var scoreMessageObject = { MessageDef: messageDef };
 		if (piCurrent.manyErrors !== '') {
